57402cd5a269871d6e17974124e9eacd23276087
AngelReina <113972511+AngelReina@users.noreply.github.com>
2022-09-20 12:12:12 +0200

    Add files via upload

---
+++ ejercicio1.2.py
@@ -0,0 +1,17 @@
+# Realizar un programa que sea capaz de convertir los grados centígrados en grados Farenheit y viceversa
+
+# Definir variables
+centigrado = 0
+farenheit = 0
+
+# Preguntar al usuario por los datos
+
+centigrado = float(input(" Introduzca los grados centigrados: "))
+
+# Hago el problema
+
+farenheit = 1.8 * centigrado + 32
+
+# Muestro el resultado
+
+print("Hace ",farenheit, "grados")
